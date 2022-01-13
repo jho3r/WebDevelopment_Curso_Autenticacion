5b0825e7e75379fd34f61739275632b6cc08f3f8
jho3r <jhoer.perez@gmail.com>
2022-01-13 00:05:57 -0500

    Se implementa el concepto de salt rounds y hsahing

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 //const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10; // salt rounds for bcrypt
 
 const app = express();
 
@@ -36,15 +39,23 @@ app.route('/login')
     })
     .post(function(req, res){
         const username = req.body.username;
-        const password = md5(req.body.password);
+        //const password = md5(req.body.password);
+        const password = req.body.password;
         User.findOne({email: username}, function(err, foundUser){
             if(err){
                 console.log(err);
             } else {
                 if(foundUser){
-                    if(foundUser.password === password){
+                    bcrypt.compare(password, foundUser.password, function(err, result){
+                        if(result){
+                            res.render("secrets");
+                        } else {
+                            res.send("Incorrect password");
+                        }
+                    });
+                    /*if(foundUser.password === password){
                         res.render("secrets");
-                    }
+                    }*/
                 }else{
                     res.send("User not found");
                 }
@@ -58,7 +69,23 @@ app.route('/register')
         res.render("register");
     })
     .post(function(req, res){
-        const user = new User({
+
+        bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+            const newUser = new User({
+                email: req.body.username,
+                password: hash
+            });
+
+            newUser.save(function(err){
+                if(err){
+                    console.log(err);
+                } else {
+                    res.render("secrets");
+                }
+            });
+        });
+
+        /*const user = new User({
             email: req.body.username,
             password: md5(req.body.password)
         });
@@ -68,9 +95,8 @@ app.route('/register')
             }else{
                 console.log(err);
             }
-        });
+        });*/
     });
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
